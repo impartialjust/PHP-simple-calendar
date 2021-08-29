67d3c867842dd0ba89652d5e0abdad121bcb4420
impartialjust <70059115+impartialjust@users.noreply.github.com>
2021-08-29 20:44:19 +0800

    add detail for further version

--- setting.php
+++ setting.php
@@ -1,4 +1,17 @@
 <?PHP
+/*
+---------------
+Brand desgining
+---------------
+*/
+$favicon = 'favicon.ico';                       //The file name of icon
+$favicontype = 'image/x-icon';                  //The MIME type of icon, can be image/x-icon, image/jpg
+$calendarname = 'Demo php simple calendar';     //Any name you want
+/*
+----------------
+database setting
+----------------
+*/
 $servername = "localhost"; //default as localhost
 $username = "phpcal"; //MUST CHANGE
 $password = "phpccc";//MUST CHANGE
@@ -17,3 +30,7 @@ $Mail["SMTPSecure"] = 'ENCRYPTION_SMTPS';                     //Enable implicit 
 $Mail["Port"]       = 465;                                    //TCP port to connect to; use 587 if you have set `$Mail["SMTPSecure"] = ENCRYPTION_STARTTLS`
 $Mail["Sender"]     = 'php@is.the.best';                      //Should be as same as the account options or any other email name you own
 $Mail["Sendername"] = 'PHP simple calender auto mailer';      //Can be any name
+$Mail["enablereply"] = 0;                                       //Enable reply-to, boolean, default as 0
+/*You will not need to fill below out if $Mail["enablereply"] is equal to 0*/
+$Mail["replyto"]    = '';                                      //Reply to email address
+$Mail["replytoname"] = '';                                      //Reply to name
